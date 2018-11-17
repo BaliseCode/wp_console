9c955bdf5c4053125fcae9f76c97c9cc0814b0fb
smdimagerie <simon@imagerie.ca>
2018-11-17 12:06:22 -0500

    Update makePostType.php
    Updated to add plural name

--- src/makePostType.php
+++ src/makePostType.php
@@ -8,15 +8,13 @@
 
 namespace Balise\Command;
 
-
 use Balise\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
-class makePostType extends Command
-{
+class makePostType extends Command {
     private $base_dir;
     private $cpt_dir;
 
@@ -27,16 +25,13 @@ class makePostType extends Command
      */
     public $output;
 
-
-    public function __construct()
-    {
+    public function __construct() {
         $this->base_dir = THEME;
-        $this->cpt_dir = $this->base_dir . '/custom_post_type';
+        $this->cpt_dir  = $this->base_dir . '/custom_post_type';
         parent::__construct();
     }
-    
-    private function createDirectory()
-    {
+
+    private function createDirectory() {
         $dir = $this->cpt_dir;
         if (!is_dir($dir)) {
             if (!mkdir($dir)) {
@@ -47,8 +42,7 @@ class makePostType extends Command
 
     }
 
-    private function createFile($stub_fields)
-    {
+    private function createFile($stub_fields) {
         $stub = file_get_contents(__DIR__ . '/stubs/customPostType.stub');
 
         if (defined('TEXT_DOMAIN')) {
@@ -63,18 +57,15 @@ class makePostType extends Command
         return file_put_contents($this->cpt_dir . '/' . $stub_fields['dummy_post_type'] . '.php', $stub);
     }
 
-
-    public function configure()
-    {
+    public function configure() {
         $this->setName('makePostType')
             ->setDescription('Create a Post Type boilerplate.')
             ->addArgument('name', InputArgument::REQUIRED, 'The name of the custom post type.')
             ->addOption('no_archive', false);
     }
 
-    public function execute(InputInterface $input, OutputInterface $output)
-    {
-        $name = $input->getArgument('name');
+    public function execute(InputInterface $input, OutputInterface $output) {
+        $name       = $input->getArgument('name');
         $no_archive = $input->getOption('no_archive');
 
         $this->output = $output;
@@ -85,7 +76,7 @@ class makePostType extends Command
         }
 
         if (is_file($this->cpt_dir . '/' . $name . '.php')) {
-            $helper = $this->getHelper('question');
+            $helper   = $this->getHelper('question');
             $question = new ConfirmationQuestion('Seems that the custom post type already exists. Overwrite it? (n/y)', false);
             if (!$helper->ask($input, $output, $question)) {
                 $this->message('info', 'Command cancelled!!');
@@ -93,25 +84,27 @@ class makePostType extends Command
             }
         }
 
-
         if (substr($name, -1) == 's') {
             $singular = ucfirst(substr($name, 0, -1));
+            $plural   = ucfirst($name);
         } else {
             $singular = ucfirst($name);
+            $plural   = ucfirst($name . 's');
         }
 
-        $is_public = 'true';
+        $is_public   = 'true';
         $has_archive = 'true';
-        if($no_archive) {
+        if ($no_archive) {
             $has_archive = 'false';
         }
 
         $stub_fields = [
-            'dummy_post_type' => $name,
-            'dummy_post_name' => ucfirst($name),
-            'dummy_singular_name' => $singular,
-            'is_public' => $is_public,
-            'post_has_archive' => $has_archive
+            'dummy_post_type'     => $name,
+            'dummy_post_name'     => ucfirst($name),
+            'Dummy_singular_name' => $singular,
+            'Dummy_plural_name'   => $plural,
+            'is_public'           => $is_public,
+            'post_has_archive'    => $has_archive,
         ];
 
         if ($this->createFile($stub_fields)) {
